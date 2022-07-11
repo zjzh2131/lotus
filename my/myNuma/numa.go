873f02c7527397dc7d74836ce43ba50139a636fe
xtzjsgfw <202741256@qq.com>
2022-07-11 00:03:24 -0700

    update:numa

--- my/myNuma/numa.go
+++ my/myNuma/numa.go
@@ -4,6 +4,7 @@ package myNuma
 //
 // #include <numa.h>
 import "C"
+import "unsafe"
 
 // IsAvailable returns true if NUMA is
 // available.
@@ -33,4 +34,37 @@ func SetPreferred(node int) {
 // to be made from the local memory.
 func SetLocalAlloc() {
 	C.numa_set_localalloc()
+}
+
+// NumaRunOnNode equal numa_run_on_node()
+func NumaRunOnNode(node int) {
+	C.numa_run_on_node(C.int(node))
+}
+
+// NumaNumTaskNodes equal numa_num_task_nodes()
+//func NumaNumTaskNodes() int {
+//	return C.numa_num_task_nodes
+//}
+
+// NumaNumTaskCpus equal numa_num_task_cpus()
+//func NumaNumTaskCpus() int {
+//	return C.numa_num_task_cpus
+//}
+
+// numa_node_size, numa_node_size64
+
+// NumaNodeSize64 equal numa_node_size64
+//func NumaNodeSize64(node int, free bool) int64 {
+//	freeP := (*C.longlong)((unsafe.Pointer)(new(int64)))
+//	return C.numa_node_size64(C.int(node), freeP)
+//}
+
+type NodeMask C.nodemask_t
+
+// NumaBind equal numa_bind()
+func NumaBind(node int) {
+	maskP := (*C.nodemask_t)((unsafe.Pointer)(new(NodeMask)))
+	C.nodemask_zero(maskP)
+	C.nodemask_set_compat(maskP, 1)
+	C.numa_bind(maskP)
 }
